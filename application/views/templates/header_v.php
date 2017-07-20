b1312040c0de160300c302dee68094b8a150648a
facmanus <fac.manus@gmail.com>
2017-07-20 23:08:50 +0900

    페이스북 로그인 적용 - 페이지 로딩 시에 적용되기.

--- application/views/templates/header_v.php
+++ application/views/templates/header_v.php
@@ -16,6 +16,71 @@
 
 <body>
 
+<script>
+
+	// This is called with the results from from FB.getLoginStatus().
+	function statusChangeCallback(response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+		// The response object is returned with a status field that lets the
+		// app know the current login status of the person.
+		// Full docs on the response object can be found in the documentation
+		// for FB.getLoginStatus().
+		if (response.status === 'connected') {
+			// Logged into your app and Facebook.
+			testAPI();
+		} else {
+			// The person is not logged into your app or we are unable to tell.
+			//document.getElementById('status').innerHTML = 'Please log ' +
+			//	'into this app.';
+		}
+	}
+
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
+
+	// Here we run a very simple test of the Graph API after login is
+	// successful.  See statusChangeCallback() for when this call is made.
+	function testAPI() {
+		console.log('Welcome!  Fetching your information.... ');
+		FB.api('/me', function(response) {
+			console.log('Successful login for: ' + response.name);
+			document.getElementById('status').innerHTML =
+				'Thanks for logging in, ' + response.name + '!';
+		});
+	}
+
+	window.fbAsyncInit = function() { //이 이하의 함수는 sdk를 모두 읽어들인 후에 실행됨.
+		FB.init({
+			appId            : '839457696217884',
+			autoLogAppEvents : true,
+			xfbml            : true,
+			version          : 'v2.10'
+		});
+
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+
+		FB.AppEvents.logPageView();
+	};
+
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		/*js.src = "//connect.facebook.net/en_US/sdk.js";*/
+		js.src = "//connect.facebook.net/ko_KR/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
+
 <nav class="navbar navbar-default navbar-static-top">
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
@@ -27,6 +92,12 @@
 				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand" href="/">CIProject</a>
+			<div
+				class="fb-like"
+				data-share="true"
+				data-width="450"
+				data-show-faces="true">
+			</div>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
@@ -60,7 +131,7 @@ if ($this->session->userdata('logged_in') == TRUE) {
 					<?php
 					//csrf 방지
 					$attributes = array('class' => 'navbar-form navbar-right', 'id' => 'auth_login', 'role' => 'login');
-					echo form_open('/index.php/auth/login', $attributes);
+					echo form_open('auth/login', $attributes);
 					?>
 					<div class="form-group">
 <!--						<label for="user_id" class="sr-only">userid</label>-->
@@ -69,11 +140,18 @@ if ($this->session->userdata('logged_in') == TRUE) {
 						<input type="text" name="password" id="password" class="form-control" placeholder="password">
 					</div>
 					<button type="submit" class="btn btn-default">Submit</button>
+					<!--facebook-->
+					<button type="button" class="btn btn-primary">페이스북로그인</button>
+
+<!--						<div class="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>-->
+						<!--<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+						</fb:login-button>-->
 <?php
 }
 ?>
 				</li>
 				<li><a href="/">Board</a></li>
+				<li><div id="status"></div></li><!--facebook 확인-->
 				<!--				<li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
